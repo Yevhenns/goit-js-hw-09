a555d585a5ebc0e04c5c69472804d1e2d10f298d
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-24 11:51:39 +0300

    first_task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
