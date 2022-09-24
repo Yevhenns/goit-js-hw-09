ad4d4b7fa011f737a4789e3af2ba137bf0f53386
Yevhenns <103356318+Yevhenns@users.noreply.github.com>
2022-09-24 22:39:47 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,6 +6,8 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+let timerId = null;
+
 startBtn.addEventListener('click', () => {
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
